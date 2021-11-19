8b5c369d57e421099bb7a7debf95ea11621f49bd
Darcy <Darcelisgutierrez@gmail.com>
2021-11-18 23:10:30 -0500

    All Basic UI elements have been integrated

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//PURPOSE: To maneuver through alll of the UI within the scene 
+//USAGE:Place on game manager object 
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject computerPanel;
+    public GameObject notepadPanel; 
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //make sure all panels are inactive at the start of the game 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //if you click on the computer, then activa
+
+    // if you click on the note pad 
+
+    
+}
