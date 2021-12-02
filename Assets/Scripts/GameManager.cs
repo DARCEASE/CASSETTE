82bb6753a721c452771d6f19bcf02a542b51ec4f
Darcy <Darcelisgutierrez@gmail.com>
2021-12-02 12:36:06 -0500

    Playtest Thursday 1 Playable - Jim Updated Scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -27,6 +28,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("UpdatedGameScene");
+        }
         
     }
     public void ExitButton()
