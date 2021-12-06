b3b1bee50af5c2123244609301fa7be780546796
Darcy <Darcelisgutierrez@gmail.com>
2021-12-05 19:18:16 -0500

    NEWSPAPER ALL OF NOVAK SUMBMISSIONS IN

--- Assets/Scripts/NewspaperBehavior.cs
+++ Assets/Scripts/NewspaperBehavior.cs
@@ -12,7 +12,15 @@ public class NewspaperBehavior : MonoBehaviour
 
     // MAKE A VARIABLE FOR THE DROPDOWN ITSELF 
     public Dropdown NovakDPOne;
-    public Text NewspaperCopy;
+    public Dropdown NovakDPTwo;
+    public Dropdown NovakDPThree;
+
+    //text Containers
+    public Text NovakNewspaperA;
+    public Text NovakNewspaperB;
+    public Text NovakNewspaperC;
+    
+
 
     // establish all string variables that can be changed. and assign them on a UI text 
 
@@ -22,31 +30,31 @@ public class NewspaperBehavior : MonoBehaviour
     public string novakInfoC; //C
 
     //Jim 
-    public Text jimInfoA; //container that will hold selected option  for A 
-    public Text jimInfoB; //B
-    public Text jimInfoC; //C
+    public string jimInfoA; //container that will hold selected option  for A 
+    public string jimInfoB; //B
+    public string jimInfoC; //C
 
     //Julia 
-    public Text juliaInfoA; //container that will hold selected option A
-    public Text juliaInfoB; //B
-    public Text juliaInfoC; //C
+    public string juliaInfoA; //container that will hold selected option A
+    public string juliaInfoB; //B
+    public string juliaInfoC; //C
 
     //Michael 
-    public Text michaelInfoA; //container that will hold selected option A
-    public Text michaelInfoB; //B
-    public Text michaelInfoC; //C
+    public string michaelInfoA; //container that will hold selected option A
+    public string michaelInfoB; //B
+    public string michaelInfoC; //C
 
     //Max
-    public Text maxInfoA; //container that will hold selected option A
-    public Text maxInfoB; //B
-    public Text maxInfoC; //C
+    public string maxInfoA; //container that will hold selected option A
+    public string maxInfoB; //B
+    public string maxInfoC; //C
 
 
     // Start is called before the first frame update
     void Start()
     {
         
-        //public text = julia outcome and - string julia info if (chose a), show this string, if (chose b), if chose c - then jula outcome == "julia info"
+       
     }
 
     // Update is called once per frame
@@ -54,26 +62,147 @@ public class NewspaperBehavior : MonoBehaviour
     {
         
     }
+
+    //NOVAK
     public void NovakDropA()
     {
         if (NovakDPOne.value == 0) // if you choose the first option for Novak Is.. 
         {
-            Debug.Log("Detective Novak is the detective assigned to the case");
-            novakInfoA = "The detective assigned to the case";// change the text in the newspaper to be a certain line
-            NewspaperCopy.text = "Mr. Robert Evergreen, local hero and CEO of Abundandia Investments, was found dead on the morning of Monday, November 25th. According to an official police statement given to this reporter by Detective Novak, the manner of death is still undetermined." + "Novak is " + novakInfoA;
-
+            Debug.Log("NOV. Drop 1 Opt 3");
+            novakInfoA = "The detective assigned to the case.";// change the text in the newspaper to be a certain line
+            NovakNewspaperA.text = "Mr. Robert Evergreen, local hero and CEO of Abundandia Investments, was found dead on the morning of Monday, November 27th. According to an official police statement given to this reporter by Detective Novak, the manner of death is still undetermined." + " Detective Novak is " + novakInfoA;
+            
         }
         else if (NovakDPOne.value == 1)
         {
-            Debug.Log("Detective Novak is the detective the case was handed off to");
-            novakInfoB = "the detective the case was handed off to";// change the text in the newspaper to be a certain line
-            NewspaperCopy.text = "Mr. Robert Evergreen, local hero and CEO of Abundandia Investments, was found dead on the morning of Monday, November 25th. According to an official police statement given to this reporter by Detective Novak, the manner of death is still undetermined." + "Novak is " + novakInfoB;
+            Debug.Log("NOV. Drop 1 Opt 2");
+            novakInfoB = "the detective the case was handed off to.";// change the text in the newspaper to be a certain line
+            NovakNewspaperA.text = "Mr. Robert Evergreen, local hero and CEO of Abundandia Investments, was found dead on the morning of Monday, November 27th. According to an official police statement given to this reporter by Detective Novak, the manner of death is still undetermined." + "Detective Novak is " + novakInfoB;
         }
         else if (NovakDPOne.value == 2)
         {
-            Debug.Log("Detective Novak is on top of the case.");
-            novakInfoC = "on top of the case";// change the text in the newspaper to be a certain line
-            NewspaperCopy.text = "Mr. Robert Evergreen, local hero and CEO of Abundandia Investments, was found dead on the morning of Monday, November 25th. According to an official police statement given to this reporter by Detective Novak, the manner of death is still undetermined."+ "Novak is " + novakInfoC;
+            Debug.Log("NOV. Drop 1 Opt 3");
+            novakInfoC = "on top of the case.";// change the text in the newspaper to be a certain line
+            NovakNewspaperA.text = "Mr. Robert Evergreen, local hero and CEO of Abundandia Investments, was found dead on the morning of Monday, November 27th. According to an official police statement given to this reporter by Detective Novak, the manner of death is still undetermined."+ " Detective Novak is " + novakInfoC ;
         }
+    }
+    public void NovakDropB() // WHO second dropdown for Novak
+    {
+        if (NovakDPTwo.value == 0) // if you choose the first option for Novak Is.. 
+        {
+            Debug.Log("NOV. Drop 2 Opt 1");
+            novakInfoB = "is passionate about getting justice.";// change the text in the newspaper to be a certain line
+            NovakNewspaperB.text = "Novak " + novakInfoB;
+        }
+        else if (NovakDPTwo.value == 1)
+        {
+            Debug.Log("NOV. Drop 2 Opt 2");
+            novakInfoB = "is determined to uncover all the facts.";// change the text in the newspaper to be a certain line
+            NovakNewspaperB.text = "Novak " + novakInfoB; 
+        }
+        else if (NovakDPTwo.value == 2)
+        {
+            Debug.Log("NOV. Drop 2 Opt 3");
+            novakInfoB = "is taking a passive stance towards solving the case.";// change the text in the newspaper to be a certain line
+            NovakNewspaperB.text = "Novak " + novakInfoB ;
+        }
+    }
+    public void NovakDropC()
+    {
+        if (NovakDPThree.value == 0) // if you choose the first option for Novak Is.. 
+        {
+            Debug.Log("NOV. Drop 3 Opt 1");
+            novakInfoC = "Novak seems more interested in gossip than finidn the truth.";// change the text in the newspaper to be a certain line
+            NovakNewspaperC.text = "While it is clear that Mr. Evergreen’s death was the apparent result of a fall from a great height, the circumstances of that fall are the subject of an active and ongoing investigation, and the NYPD does not wish to speculate on those circumstances at this time." +  "But " + novakInfoC;
+        }
+        else if (NovakDPThree.value == 1)
+        {
+            Debug.Log("NOV. Drop 3 Opt 2");
+            novakInfoC = "Novak is determined to uncover the thruth, no matter what.";// change the text in the newspaper to be a certain line
+            NovakNewspaperC.text = "While it is clear that Mr. Evergreen’s death was the apparent result of a fall from a great height, the circumstances of that fall are the subject of an active and ongoing investigation, and the NYPD does not wish to speculate on those circumstances at this time."  + "But " + novakInfoC;
+          
+        }
+        else if (NovakDPThree.value == 2)
+        {
+            Debug.Log("NOV. Drop 3 Opt 3");
+            novakInfoC = "Novak is following up on any and all leads.";// change the text in the newsaper to be a certain line
+            NovakNewspaperC.text = "While it is clear that Mr. Evergreen’s death was the apparent result of a fall from a great height, the circumstances of that fall are the subject of an active and ongoing investigation, and the NYPD does not wish to speculate on those circumstances at this time." + "But " + novakInfoC;
+        }
+    }
+
+    //JIM 
+    public void JimDropA()
+    {
+
+    }
+    public void JimDropB()
+    {
+
+    }
+    public void JimDropC()
+    {
+        /*
+         // TEMPLATE FOR ALL CHARS AND DROPDOWNS
+        if (Char_NameDPThree.value == 0) // 3rd dropdown
+        {
+            Debug.Log("Char_Name. Drop 3 Opt 1");
+            Char_NameDPInfoC = "";// change the text in the newspaper to be a certain line
+            Char_NameDPNewspaperC.text = "" + "And " + Char_NameDPInfoC;
+        }
+        else if (Char_NameDPThree.value == 1)
+        {
+            Debug.Log("Char_Name. Drop 3 Opt 2");
+            Char_NameDPInfoC = "";// change the text in the newspaper to be a certain line
+            Char_NameNewspaperC.text = "" + "And " + Char_NameInfoC;
+        }
+        else if (Char_NameDPThree.value == 2)
+        {
+            Debug.Log("Char_Name. Drop 3 Opt 3");
+            Char_NameInfoC = "";// change the text in the newsaper to be a certain line
+            Char_NameNewspaperC.text = "" + "And " + Char_NameInfoC;
+        }*/
+    }
+
+    //JULIA 
+
+    public void JuliaDropA()
+    {
+
+    }
+    public void JuliaDropB()
+    {
+
+    }
+    public void JuliaDropC()
+    {
+
+    }
+
+    //MICHAEL 
+    public void MichaelDropA()
+    {
+
+    }
+    public void MichaelDropB()
+    {
+
+    }
+    public void MichaelDropC()
+    {
+
+    }
+
+    //MAX
+    public void MaxDropA()
+    {
+
+    }
+    public void MaxDropB()
+    {
+
+    }
+    public void MaxDropC()
+    {
+
     }
 }
