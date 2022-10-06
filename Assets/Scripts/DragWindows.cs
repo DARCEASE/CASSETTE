0813476a1c737773521499ae3a5c43249a4315ba
Darcy <Darcelisgutierrez@gmail.com>
2022-10-06 10:04:50 -0400

    Auto stash before merge of "DarcyPC" and "origin/AshBranch"

---
+++ Assets/Scripts/DragWindows.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+//PURPOSE: Allow you to click and drag UI windows
+//USAGE: place this script on THE game manager? or the canvas itself
+public class DragWindows : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+    [SerializeField] private RectTransform dragRectTransform;
+    [SerializeField] private Canvas canvas;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (dragRectTransform == null)
+        {
+            dragRectTransform = transform.parent.GetComponent<RectTransform>();
+        }
+        if (canvas == null)
+        {
+            Transform testCanvasTransform = transform.parent;
+            while (testCanvasTransform != null) //if empty, grab the nearest and most vaialbale canvas
+            {
+                canvas = testCanvasTransform.GetComponent<Canvas>();
+                if (canvas != null)
+                {
+                    break;
+                }
+                testCanvasTransform = testCanvasTransform.parent;
+            }
+        }
+    }
+ 
+   
+
+    void IDragHandler.OnDrag(PointerEventData eventData) // as youre draggging the mouse
+    {
+        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor; //grabs mvmt form previous mouse pos
+
+        Debug.Log("PlayerDraggingMouse");
+    }
+    public void OnPointerDown(PointerEventData eventData) // as you click and hold 
+    {
+        dragRectTransform.SetAsLastSibling();
+
+        Debug.Log("everythingOnTop");
+    }
+}
