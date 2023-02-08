685ece7acca8c44d1a233997f91dc3a43faa5d68
AshleyE <ase330@nyu.edu>
2023-02-08 16:25:11 -0500

    //Borders in, cursor issue fixed!

--- Assets/Scripts/DragWindows.cs
+++ Assets/Scripts/DragWindows.cs
@@ -8,11 +8,13 @@ using UnityEngine.EventSystems;
 public class DragWindows : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
     [SerializeField] private RectTransform dragRectTransform;
+    float DragXPos, DragYPos;
     [SerializeField] private Canvas canvas;
 
     // Start is called before the first frame update
     private void Awake()
     {
+        Cursor.lockState = CursorLockMode.Confined;
         if (dragRectTransform == null)
         {
             dragRectTransform = transform.parent.GetComponent<RectTransform>();
@@ -22,6 +24,7 @@ public class DragWindows : MonoBehaviour, IDragHandler, IPointerDownHandler
             Transform testCanvasTransform = transform.parent;
             while (testCanvasTransform != null) //if empty, grab the nearest and most vaialbale canvas
             {
+                
                 canvas = testCanvasTransform.GetComponent<Canvas>();
                 if (canvas != null)
                 {
@@ -33,13 +36,38 @@ public class DragWindows : MonoBehaviour, IDragHandler, IPointerDownHandler
     }
  
    
-
     void IDragHandler.OnDrag(PointerEventData eventData) // as youre draggging the mouse
     {
-        
-        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor; //grabs mvmt form previous mouse pos
+
+        DragXPos = (dragRectTransform.anchoredPosition.x + eventData.delta.x) / canvas.scaleFactor;
+        DragYPos = (dragRectTransform.anchoredPosition.y + eventData.delta.y) / canvas.scaleFactor;
+
+        if (DragXPos <= -200) //less than
+        {
+            DragXPos = -200;
+        }
+ 
+        if (DragXPos >= 430) //greater than
+        {
+            DragXPos = 430;
+        }
+ 
+        if (DragYPos >= 15) //less than
+        {
+            DragYPos = 15;
+        }
+ 
+        if (DragYPos <= -200)
+        {
+            DragYPos = -200;
+        }
+
+        dragRectTransform.anchoredPosition = new Vector2(DragXPos, DragYPos);
+        //dragRectTransform.anchoredPosition = dragRectTransform.anchoredPosition + eventData.delta / canvas.scaleFactor; //grabs mvmt form previous mouse pos
+        //dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor; //grabs mvmt form previous mouse pos
         //windowpos.y = Mathf.Clamp(windowpos.y, 30, 15);
         Debug.Log("PlayerDraggingMouse");
+    
     }
     public void OnPointerDown(PointerEventData eventData) // as you click and hold 
     {
