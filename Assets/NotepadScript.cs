df4170e4bcf39f266d2248bbb3c18b4f64e23ced
AshleyE <ase330@nyu.edu>
2023-04-05 14:50:35 -0400

    //Anchored and tweaked notepad

--- Assets/NotepadScript.cs
+++ Assets/NotepadScript.cs
@@ -7,7 +7,8 @@ using TMPro;
 public class NotepadScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] float rectX, rectY, rectW, rectH;
+    [SerializeField] GameObject notepadPanel;
+    [SerializeField] float rectW, rectH, testVal;
     GUIStyle currentStyle;
     [SerializeField] string userText = "Write notes here.";
     void Start()
@@ -18,7 +19,7 @@ public class NotepadScript : MonoBehaviour
     // Update is called once per frame
     void OnGUI()
     {
-        userText = GUI.TextArea(new Rect(rectX, rectY, rectW, rectH), userText);
+        userText = GUI.TextArea(new Rect(notepadPanel.transform.position.x / testVal, notepadPanel.transform.position.y / testVal, rectW, rectH), userText);
         //GUI.skin.box.normal.color = Color.gray;
     }
 }
