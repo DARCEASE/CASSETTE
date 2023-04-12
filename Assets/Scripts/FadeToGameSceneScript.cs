02a9514d7c130e9b17e6551d0f2b64359b9d986a
AshleyE <ase330@nyu.edu>
2023-04-12 15:58:32 -0400

    //Updated Papers in S2

--- Assets/Scripts/FadeToGameSceneScript.cs
+++ Assets/Scripts/FadeToGameSceneScript.cs
@@ -4,36 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-
-public class TransitionScript : MonoBehaviour
+public class FadeToGameSceneScript : MonoBehaviour
 {
- 
     [SerializeField] CanvasGroup canvas;
     Scene scene;
     [SerializeField] Image TransitionOBJ;
     [SerializeField] float transitionTime;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        scene = SceneManager.GetActiveScene();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-    
-    public void FadeOutScene(){
         StartCoroutine(IntroLevel());
     }
-
-    public void ToNextScene(){
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-    }
-    //One to Fade in...
-    //One to Fade Out...
 
     IEnumerator IntroLevel(){
         float t = 1;
@@ -42,19 +30,5 @@ public class TransitionScript : MonoBehaviour
             canvas.alpha = t;
             yield return null;
         }
-    }
-
-    IEnumerator LoadLevel(int levelIndex){
-        float t = 0;
-        while ( t < transitionTime){
-            t += Time.deltaTime;
-            canvas.alpha = t/transitionTime;
-            yield return null;
-        }        
-
-        if (levelIndex > 5){
-            levelIndex = 0;
-        } 
-        SceneManager.LoadScene(levelIndex);
     }
 }
