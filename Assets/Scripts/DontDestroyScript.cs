c83efa63ea2fb74d4922292bca8c30dacb0869a6
AshleyE <ase330@nyu.edu>
2023-05-12 16:03:52 -0400

    //Scene Fixed!

--- Assets/Scripts/DontDestroyScript.cs
+++ Assets/Scripts/DontDestroyScript.cs
@@ -9,18 +9,8 @@ DON'T REMOVE/DESTROY IT OR ELSE ERRORS WILL HAPPEN!
 
 public class DontDestroyScript : MonoBehaviour
 {
-    Scene scene;
     void Update()
     {
-        scene = SceneManager.GetActiveScene();
         DontDestroyOnLoad(this.gameObject);
-
-        /*if (scene.name == "TESTStoryOneScene 1" || scene.name == "TESTStoryTwoScene 1"){
-            Debug.Log("Scene Name DDS: " + scene.name);
-            this.gameObject.SetActive(true);
-        } else{
-        //if (scene.name == "ToStoryTwo" || scene.name == "EndScene"){
-            this.gameObject.SetActive(false);
-        } */   
     }
 }
---
+++ Assets/ToggleGO.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToggleGO : MonoBehaviour
+{
+
+    [SerializeField] GameObject GameItems;
+    Scene scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scene = SceneManager.GetActiveScene();
+        DontDestroyOnLoad(this.gameObject);
+
+        if (scene.name == "TESTStoryOneScene 1" || scene.name == "TESTStoryTwoScene 1"){
+            Debug.Log("Scene Name Toggle: " + scene.name);
+            GameItems.SetActive(true);
+        } else{
+        //if (scene.name == "ToStoryTwo" || scene.name == "EndScene"){
+            GameItems.SetActive(false);
+        }
+ 
+    }
+}
