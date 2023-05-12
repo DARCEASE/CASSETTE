3d7a7f00b8b221e7182bbbd7b147c3866874d044
AshleyE <ase330@nyu.edu>
2023-05-12 14:59:04 -0400

    //Potential Windows Build

--- Assets/Scripts/DontDestroyScript.cs
+++ Assets/Scripts/DontDestroyScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 /**
 This script is ONLY for keeping objs alive after someone hits replay. 
 DON'T REMOVE/DESTROY IT OR ELSE ERRORS WILL HAPPEN!
@@ -8,8 +9,13 @@ DON'T REMOVE/DESTROY IT OR ELSE ERRORS WILL HAPPEN!
 
 public class DontDestroyScript : MonoBehaviour
 {
+    Scene scene;
     void Update()
     {
-        DontDestroyOnLoad(this.gameObject);
+        if (scene.name != "EndScene"){
+            DontDestroyOnLoad(this.gameObject);
+        } else{
+            this.gameObject.SetActive(false);
+        }
     }
 }
